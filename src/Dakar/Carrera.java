bf50a0f7181dbf5ccccec796e4e22a6db2562dba
Francisco Foukal <francisco.foukal@mercadolibre.com>
2021-01-26 12:22:22 -0300

    se agrega clase Carrera

---
+++ src/Dakar/Carrera.java
@@ -0,0 +1,28 @@
+package Dakar;
+
+import java.util.List;
+
+public class Carrera {
+    private Double distancia;
+    private Integer premioEnDolares;
+    private String nombre;
+    private Integer cantidadDeVehiculosPermitidos;
+    private List<Vehiculo> vehiculoList;
+
+    public Carrera(Double distancia, Integer premioEnDolares, String nombre, Integer cantidadDeVehiculosPermitidos, List<Vehiculo> vehiculoList) {
+        this.distancia = distancia;
+        this.premioEnDolares = premioEnDolares;
+        this.nombre = nombre;
+        this.cantidadDeVehiculosPermitidos = cantidadDeVehiculosPermitidos;
+        this.vehiculoList = vehiculoList;
+    }
+
+
+    public void darDeAltaAuto(int velocidad, double aceleracion, double AnguloDeGiro, String patente){
+
+    }
+
+    public void darDeAltaMoto(int velocidad, double aceleracion, double AnguloDeGiro, String patente){
+
+    }
+}
