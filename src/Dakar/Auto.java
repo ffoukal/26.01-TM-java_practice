a6b41df55fe4dbedc6fb010f491a7cd143b94f9e
ignacio probst <ignacio.probst@mercadolibre.com>
2021-01-26 12:22:27 -0300

    Se agrego clase auto, moto y metodos correspondientes

---
+++ src/Dakar/Auto.java
@@ -0,0 +1,7 @@
+package Dakar;
+
+public class Auto extends  Vehiculo{
+    public Auto(int velocidad, double aceleracion, double anguloDeGiro, String patente, double peso, int ruedas) {
+        super(velocidad, aceleracion, anguloDeGiro, patente, 1000, 4);
+    }
+}
---
+++ src/Dakar/Moto.java
@@ -0,0 +1,8 @@
+package Dakar;
+
+public class Moto extends Vehiculo{
+
+    public Moto(int velocidad, double aceleracion, double anguloDeGiro, String patente) {
+        super(velocidad, aceleracion, anguloDeGiro, patente, 300, 2);
+    }
+}
