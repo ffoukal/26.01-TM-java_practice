0a77974ca8f75f6a936f60366faa167f28567437
Francisco Foukal <francisco.foukal@mercadolibre.com>
2021-01-26 13:15:35 -0300

    se agrega funcion getganador

--- src/Dakar/Carrera.java
+++ src/Dakar/Carrera.java
@@ -1,7 +1,9 @@
 package Dakar;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Carrera {
     private Double distancia;
@@ -38,6 +40,23 @@ public class Carrera {
     public void eliminarVehiculoConPatente(String unaPatente){
         Vehiculo vehiculoABuscar = (Vehiculo) vehiculoList.stream().filter(vehiculo -> vehiculo.getPatente().compareTo(unaPatente) == 0);
         eliminarVehiculo(vehiculoABuscar);
+    }
+
+    public Vehiculo getGanador(){
+
+        //FORMULA =  Velocidad * 1⁄2 Aceleracion / (AnguloDeGiro*(Peso-Cantidad de Ruedas * 100)
+
+        Map<Double, Vehiculo> results = new HashMap<>();
+
+        for (Vehiculo vehiculo : vehiculoList){
+            double result = vehiculo.getVelocidad() * 0.5 * vehiculo.getAceleracion() / (vehiculo.getAnguloDeGiro()* (vehiculo.getPeso() - vehiculo.getRuedas() * 100));
+            results.put(result, vehiculo);
+        }
+
+        double max = results.keySet().stream().max(Double::compare).get();
+
+        return results.get(max);
+
     }
 
 }
