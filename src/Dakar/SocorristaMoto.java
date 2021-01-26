78e789858a2242f8c7419191ab31db8477588fb7
ignacio probst <ignacio.probst@mercadolibre.com>
2021-01-26 13:04:41 -0300

    Se agrego las clases SocorristaAuto y SocorristaMoto

---
+++ src/Dakar/SocorristaMoto.java
@@ -0,0 +1,8 @@
+package Dakar;
+
+public class SocorristaMoto {
+
+    public void socorrer(Moto unaMoto){
+        System.out.println("Socorriendo moto con patente: " + unaMoto.getPatente());
+    }
+}
