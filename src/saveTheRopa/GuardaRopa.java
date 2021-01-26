8713a1fa5c414513ee0ae59d4aab8e0870c8014c
ignacio probst <ignacio.probst@mercadolibre.com>
2021-01-26 11:21:15 -0300

    se agrega clase guardaropa

---
+++ src/saveTheRopa/GuardaRopa.java
@@ -0,0 +1,25 @@
+package saveTheRopa;
+
+
+import java.util.List;
+import java.util.Map;
+
+public class GuardaRopa {
+
+    private Map<Integer, List<Prenda>> diccionario;
+    private Integer contador;
+
+    public Integer guardarPrendas(List<Prenda> listaDePrenda){
+        this.diccionario.put(contador, listaDePrenda);
+        return contador++;
+    }
+
+    public void mostrarPrendas(){
+        for (Map.Entry<Integer, List<Prenda>> entry : diccionario.entrySet()){
+            System.out.println( "GuardaRopa N" + entry.getKey() + " - " + entry.getValue().toString());
+        }
+    }
+
+
+
+}
--- src/saveTheRopa/Prenda.java
+++ src/saveTheRopa/Prenda.java
@@ -1,4 +1,6 @@
 package saveTheRopa;
 
 public class Prenda {
+
+
 }
